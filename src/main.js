75be3dcb5b69471ab77d8b76bcba15961414a874
Tyson Lackey <lackeyt90@gmail.com>
2020-07-03 02:24:16 -0700

    update HTML index and API call

--- src/main.js
+++ src/main.js
@@ -2,23 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import {exchangeRateCall} from 'exchangeRateCall.js'
+import {exchangeRateCall} from './exchangeRateCall.js';
 
 async function exchangeRateResponse() {
   const responseObject = await exchangeRateCall(); // blocking // api call
-  if(!wordArray) {
+  if(!responseObject) {
     return 'There has been an error processing your request';
   } else {
     if (responseObject.result === "error"){
-      return responseObject.error-type
+      return responseObject[1].val();
     } else if (responseObject.result === "success") {
-      let conversionRates = responseObject.conversion_rates
-      return conversionRates
+      let conversionRates = responseObject.conversion_rates;
+      return conversionRates;
     }
   }
 }
 
-$(document).ready(function() {
+$(document).ready(async function() {
   //User Interface
+  let response = await exchangeRateResponse();
+  $("output").html(`${response}`);
 });
 

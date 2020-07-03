05bc154ed711983e229d3055489dc4137160d36a
Tyson Lackey <lackeyt90@gmail.com>
2020-07-03 01:49:51 -0700

    create initial API call functions

---
+++ src/exchangeRateCall.js
@@ -0,0 +1,14 @@
+export async function exchangeRateCall(){
+  try{
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/***REMOVED***/latest/USD`); 
+    let jsonifiedResponse;
+    if (response.ok && response.status == 200) {
+      jsonifiedResponse = await response.json();
+    } else {
+      jsonifiedResponse = false;
+    }
+    return jsonifiedResponse;
+  } catch (error) {
+    return false;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import {exchangeRateCall} from 'exchangeRateCall.js'
+
+async function exchangeRateResponse() {
+  const responseObject = await exchangeRateCall(); // blocking // api call
+  if(!wordArray) {
+    return 'There has been an error processing your request';
+  } else {
+    if (responseObject.result === "error"){
+      return responseObject.error-type
+    } else if (responseObject.result === "success") {
+      let conversionRates = responseObject.conversion_rates
+      return conversionRates
+    }
+  }
+}
 
 $(document).ready(function() {
   //User Interface

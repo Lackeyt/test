3a429d93c10c0fb786073636ed57f5fbdf227f0c
Tyson Lackey <lackeyt90@gmail.com>
2020-07-03 11:12:24 -0700

    update API call for multiple source currencies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import './styles.css';
 import $ from "jquery";
 import {exchangeRateCall} from './exchangeRateCall.js';
 
-async function exchangeRateResponse(usd, currency) {
+async function exchangeRateResponse(amount, currencyFrom, currencyTo) {
   const responseObject = await exchangeRateCall(); // blocking // api call
   if(!responseObject) {
     $("#output").html(`There has been an error processing your request`);
@@ -13,8 +13,8 @@ async function exchangeRateResponse(usd, currency) {
       $("#output").html(`The request returned an error: ${responseObject["error-type"]}`);
     } else if (responseObject.result === "success") {
       let conversionRates = responseObject.conversion_rates;
-      let convertedCurrency = usd * conversionRates[currency]
-      $("#output").html(`$${usd} = ${convertedCurrency} ${currency}`);
+      let convertedCurrency = amount * conversionRates[currencyTo]
+      $("#output").html(`$${amount} = ${convertedCurrency} ${currencyTo}`);
     }
   }
 }
